43718d4295154e0fe7580bc7e27c7f0bd63f681f
vegos <maglaras@gmail.com>
2018-01-03 17:09:12 +0200

    updated version - working as of 3/01/18

--- clid.php
+++ clid.php
@@ -1,9 +1,9 @@
 <?php
 // CLID.php -- 11888.gr reverse phone lookup
-function file_get_the_contents($url) 
+function get_contents($url) 
 {
   $ch = curl_init();
-  $timeout = 5; // zero for no timeout
+  $timeout = 5; // Zero (0) for no timeout
   curl_setopt ($ch, CURLOPT_URL, $url);
   curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
   curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
@@ -16,16 +16,18 @@ if (isset($argv[1]))
 {
   $phone = substr($argv[1],0,10);
   $url = "https://www.11888.gr/search/?mode=ts&phone=".$phone;
-  $text = file_get_the_contents($url) ;
+  $text = get_contents($url) ;
   preg_match("'class=\"btn-line-right\">(.*?)</a>'",$text,$temp);
   if (isset($temp[1]))
   {
     echo ltrim($temp[1]);
+    // ----- START of address information -----
     preg_match("'<p class=\"loc\">(.*?)</p>'s",$text,$temp2); // get address
     if (isset($temp2[1]))
     {    
       echo "\n".rtrim(ltrim($temp2[1]));
     }
+    // ----- END of address information -----
   }
   else
   {
